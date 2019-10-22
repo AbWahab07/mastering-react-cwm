a0a24de8cd43ae2663119740d0a090fb7f42731f
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 21:01:41 +0500

    Arrow Functions and this
    Arrow functions don't bind 'this'.
    When we pass an arrow function as a callback 'this' is inherited.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,29 +1,20 @@
-const square = function(name) {
-  return number * number;
+const person = {
+  talk() {
+    setTimeout(function() {
+      // we're calling a standalone function so 'this' isn't binded
+      // in case of callback functions strict mode doesn't kicks in and we get 'Window' object
+      console.log("this", this); // Logs 'Window' object
+    }, 1000);
+  }
 };
+person.talk();
 
-const squareArr = number => {
-  return number * number;
+const personArr = {
+  talk() {
+    setTimeout(() => {
+      // passing an arrow function as a callback will inherit 'this' keyword
+      console.log("this", this); // Logs personArr object
+    }, 1000);
+  }
 };
-
-// if there is one parameter, we can remove the parenthesis
-// if body of the function contains only one statement then
-// we can remove the curly braces as well as the return keyword
-const squareArrow = number => number * number;
-console.log(squareArrow(5));
-
-// Usage
-const jobs = [
-  { id: 1, isActive: true },
-  { id: 2, isActive: true },
-  { id: 3, isActive: false }
-];
-
-const activeJobs = jobs.filter(function(job) {
-  return job.isActive;
-});
-
-const inActiveJobs = jobs.filter(job => !job.isActive);
-
-console.log(activeJobs);
-console.log(inActiveJobs);
+personArr.talk();
