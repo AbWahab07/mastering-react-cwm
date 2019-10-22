85723130379587ed3be4db68d462bccd10545dce
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 21:09:38 +0500

    Array.map Method
    Array.map is used to render list of items in REACT. map returns a new array. It doesn't modifies the original array.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,20 +1,3 @@
-const person = {
-  talk() {
-    setTimeout(function() {
-      // we're calling a standalone function so 'this' isn't binded
-      // in case of callback functions strict mode doesn't kicks in and we get 'Window' object
-      console.log("this", this); // Logs 'Window' object
-    }, 1000);
-  }
-};
-person.talk();
-
-const personArr = {
-  talk() {
-    setTimeout(() => {
-      // passing an arrow function as a callback will inherit 'this' keyword
-      console.log("this", this); // Logs personArr object
-    }, 1000);
-  }
-};
-personArr.talk();
+const colors = ["red", "green", "blue"];
+const items = colors.map(color => `<li>${color}</li>`); // arrow function as a callback to map method
+console.log(items); // array of li
