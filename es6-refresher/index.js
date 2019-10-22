4e31aad52eb654ec5e180f9aec6ebfda694e36b2
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 20:45:43 +0500

    Arrow Functions

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,16 +1,29 @@
-const person = {
-  // property
-  name: "Abdul",
-  // methods
-  walk: function() {},
-  // ES6 method syntax
-  talk() {
-    console.log(this);
-  }
+const square = function(name) {
+  return number * number;
 };
 
-person.talk(); // logs person object
+const squareArr = number => {
+  return number * number;
+};
 
-// binding this to person object
-const talk = person.talk.bind(person);
-talk(); // logs person object
+// if there is one parameter, we can remove the parenthesis
+// if body of the function contains only one statement then
+// we can remove the curly braces as well as the return keyword
+const squareArrow = number => number * number;
+console.log(squareArrow(5));
+
+// Usage
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false }
+];
+
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive;
+});
+
+const inActiveJobs = jobs.filter(job => !job.isActive);
+
+console.log(activeJobs);
+console.log(inActiveJobs);
