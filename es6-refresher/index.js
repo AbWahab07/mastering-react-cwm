948b373441d4fd71b1da133d3663b1418392e256
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 16:48:07 +0500

    Objects
    used Object literal {} syntax to define an object.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,16 +1,14 @@
-// var -> function scope
-// let -> block scope
-// const -> block scope
+const person = {
+  // property
+  name: "Abdul",
+  // methods
+  walk: function() {},
+  // ES6 method syntax
+  talk() {}
+};
 
-function sayHello() {
-  // for (var i = 0; i < 5; i++) {
-  //   console.log(i);
-  // }
-  // console.log(i); // 5
-
-  for (let i = 0; i < 5; i++) {
-    console.log(i);
-  }
-  console.log(i); // Error i isn't defined
-}
-sayHello();
+person.talk();
+// you use bracket notation when you don't know ahead of time
+// what property or mehtod you're going to access.
+const targetMember = "name";
+person[targetMember] = "John";
