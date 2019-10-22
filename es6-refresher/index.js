f4b2b6e7eac23ea4f3444ddc11f9af74ed1ef75a
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 21:21:37 +0500

    Spread Operator
    Can be used to combine two arrays or objects. Can be used to clone an array or an object.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,8 +1,15 @@
-const address = {
-  street: "",
-  city: "",
-  country: ""
-};
+// Spread Operator usage in Arrays
+// const first = [ 1, 2, 3 ];
+// const second = [ 4, 5, 6 ];
+// const clone = [ ...first ];
+// const combined = [ ...first, ...second ];
 
-const { street } = address; // object destructring
-const { city: ct, country } = address; // Alias use
+// Spread Operator usage in Objects
+const first = { name: "Abdul" };
+const second = { job: "Developer" };
+
+const clone = { ...first }; // cloning an object
+console.log(clone);
+
+const combined = { ...first, ...second, location: "Aus" }; // combining two objects
+console.log(combined);
