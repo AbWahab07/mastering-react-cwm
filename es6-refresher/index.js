c758477ee22c6a177dff957ac879a7ccf6d90b55
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 21:35:29 +0500

    Inheritance
    uses 'extends' keyword. we should call super() inside child constructor.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -10,5 +10,17 @@ class Person {
   }
 }
 
-const person = new Person("Abdul");
-person.walk();
+// Inheritane
+class Teacher extends Person {
+  constructor(name, degree) {
+    super(name); // we must call the parent constructor otherwise there will be error
+    this.degree = degree;
+  }
+
+  teach() {
+    console.log("Teach");
+  }
+}
+
+const teacher = new Teacher("Abdul", "Software");
+teacher.walk(); // calling a method inherited from parent
