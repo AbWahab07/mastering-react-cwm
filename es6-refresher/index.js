54fa940445de2a5cdae1fd719da22a6620b62f05
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 17:06:31 +0500

    Binding this
    functions in JS are objects. Called bind method on the function(basically an Object) to bind 'this'

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -8,9 +8,9 @@ const person = {
     console.log(this);
   }
 };
-// when we call method on an object, this referes to the current object
+
 person.talk(); // logs person object
 
-const talk = person.talk;
-// when we call a standalone function, this refers to the Window
-talk(); // logs Window or undefined(if strict mode is enabled)
+// binding this to person object
+const talk = person.talk.bind(person);
+talk(); // logs person object
