8ffd468197314ccff5be86edffa2b47af01d2f17
Abdul Wahab <abwahab708@gmail.com>
2019-10-22 21:28:03 +0500

    Classes
    When we've an object with at least one method, we need a blueprint to create objects of that type and that's when we use classes.

--- es6-refresher/index.js
+++ es6-refresher/index.js
@@ -1,15 +1,14 @@
-// Spread Operator usage in Arrays
-// const first = [ 1, 2, 3 ];
-// const second = [ 4, 5, 6 ];
-// const clone = [ ...first ];
-// const combined = [ ...first, ...second ];
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
 
-// Spread Operator usage in Objects
-const first = { name: "Abdul" };
-const second = { job: "Developer" };
+  // there is a single place we've to modify walk method
+  // DRY code
+  walk() {
+    console.log("walk");
+  }
+}
 
-const clone = { ...first }; // cloning an object
-console.log(clone);
-
-const combined = { ...first, ...second, location: "Aus" }; // combining two objects
-console.log(combined);
+const person = new Person("Abdul");
+person.walk();
