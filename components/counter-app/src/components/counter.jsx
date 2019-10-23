eb118b797e0ac6f1c3d506844c42e6e2da0e1759
Abdul Wahab <abwahab708@gmail.com>
2019-10-23 18:35:27 +0500

    Passing Event Arguments
    we use inline arrow function to pass the arguments to the event handlers

--- components/counter-app/src/components/counter.jsx
+++ components/counter-app/src/components/counter.jsx
@@ -15,7 +15,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement({ id: 1 })}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -36,7 +36,8 @@ class Counter extends Component {
   }
 
   // converted into arrow function
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({ count: this.state.count + 1 });
   };
 }
