d68356df06180006d651465a411fc49b384bbfc9
Abdul Wahab <abwahab708@gmail.com>
2019-10-23 17:05:02 +0500

    Setting Attributes
    Set img src attribute dynamically using the state property. Learned how to set class and style attributes in components.

--- components/counter-app/src/components/counter.jsx
+++ components/counter-app/src/components/counter.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from "react";
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    imgURL: "https://picsum.photos/200"
   };
+
+  // property to set the style attribute
+  styles = {
+    fontSize: 20 // Css properties in Camel case
+  };
+
   render() {
     return (
-      <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
-      </React.Fragment>
+      <div>
+        <img src={this.state.imgURL} alt="" />
+        {/* Inline style using property */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+
+        {/* Inline style using an object */}
+        <button style={{ fontSize: 40 }} className="btn btn-secondary btn-sm">
+          Increment
+        </button>
+      </div>
     );
   }
 
