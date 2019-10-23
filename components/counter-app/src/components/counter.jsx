b252a5897fc5d2e0032e2c73f76844b9eb9b1a1c
Abdul Wahab <abwahab708@gmail.com>
2019-10-23 18:04:09 +0500

    Conditional Rendering - If there are no tags in a array
    Deleted helper methods getBadgeClasses and formatCount. Removed the button and span to keep the file clean. Added a helper method renderTags.

--- components/counter-app/src/components/counter.jsx
+++ components/counter-app/src/components/counter.jsx
@@ -6,42 +6,25 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
-  // property to set the style attribute
-  styles = {
-    fontSize: 20 // Css properties in Camel case
-  };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
       <div>
-        <img src={this.state.imgURL} alt="" />
-        {/* Inline style using property */}
-        <span style={this.styles} className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-
-        {/* Inline style using an object */}
-        <button style={{ fontSize: 40 }} className="btn btn-secondary btn-sm">
-          Increment
-        </button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </div>
     );
-  }
-
-  getBadgeClasses() {
-    let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
   }
 }
 
