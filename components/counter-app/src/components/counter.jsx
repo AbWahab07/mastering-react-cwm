44a01db602dd9e2be43b388035ff1e545b336143
Abdul Wahab <abwahab708@gmail.com>
2019-10-23 14:26:42 +0500

    Specifying Children
    React.Fragment is used to specify multiple elements/children in the render method.

--- components/counter-app/src/components/counter.jsx
+++ components/counter-app/src/components/counter.jsx
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
