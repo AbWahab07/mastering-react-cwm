899ec9b4002c8a85ef46341fe6fc4210e5f5c0c4
Abdul Wahab <abwahab708@gmail.com>
2019-10-23 20:39:46 +0500

    Vidly Project
    Installed font-awesome@4.7.0
    Using the starter template of bootstrap.
    Modified the App component to use the starter template. Implemented the movie component.

---
+++ components/vidly/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <main className="container">
+      <h1>Hello World</h1>
+    </main>
+  );
+}
+
+export default App;
---
+++ components/vidly/src/components/movie.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { getMovies } from "../services/fakeMovieService";
+
+class Movies extends Component {
+  state = {
+    movies: getMovies()
+  };
+
+  handleDelete = movie => {
+    // let { _id } = movie;
+    // let movies = this.state.movies;
+    // let index = movies.findIndex(m => m._id === _id);
+    // movies.splice(index, 1);
+    let movies = this.state.movies.filter(m => m._id !== movie._id);
+    this.setState({ movies });
+  };
+
+  render() {
+    return this.renderMovies();
+  }
+
+  renderMovies() {
+    if (this.state.movies.length === 0)
+      return "There are no movies in the database";
+
+    return (
+      <div>
+        <p>Showing {this.state.movies.length} movies in the database</p>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.movies.map(movie => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)}
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default Movies;
