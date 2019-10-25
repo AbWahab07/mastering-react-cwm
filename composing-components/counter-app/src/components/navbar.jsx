c15c6179a7c53a2697a82d7208f9215143516699
Abdul Wahab <abwahab708@gmail.com>
2019-10-25 18:48:21 +0500

    Stateless functional component
    NavBar component only have render mehtod, we don't have any state, any event handlers or helper methods and are getting all the data via props. In this situation we can use Stateless functional component. It simply returns the react element. We've to add props as a parameter to stateless functional component as this.props only works with class components.

--- composing-components/counter-app/src/components/navbar.jsx
+++ composing-components/counter-app/src/components/navbar.jsx
@@ -1,15 +1,13 @@
 import React, { Component } from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar {this.props.totalCounters}
-        </a>
-      </nav>
-    );
-  }
-}
+const NavBar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar {props.totalCounters}
+      </a>
+    </nav>
+  );
+};
 
 export default NavBar;
