26563a48fb9c4c101001bf519c3e788e6cfae5e6
Abdul Wahab <abwahab708@gmail.com>
2019-10-29 15:22:48 +0500

    Pagination - Handling Pages
    Passed currentPage from Movie component, and updated the class 'active' dynamically based on the page.

--- components/vidly/src/components/common/pagination.jsx
+++ components/vidly/src/components/common/pagination.jsx
@@ -5,7 +5,7 @@ import _ from "lodash"; // optimized version of Underscore
 // Events: onClick(onPageChange)
 
 const Pagination = props => {
-  const { itemsCount, pageSize } = props;
+  const { itemsCount, pageSize, currentPage, onPageChange } = props;
   const pagesCount = Math.ceil(itemsCount / pageSize); // converting floating number to an integer
 
   // Edge case: if pageSize is 10
@@ -16,8 +16,15 @@ const Pagination = props => {
     <nav>
       <ul className="pagination">
         {pages.map(page => (
-          <li key={page} className="page-item">
-            <a className="page-link" href="#">
+          <li
+            key={page}
+            className={page === currentPage ? "page-item active" : "page-item"}
+          >
+            <a
+              className="page-link"
+              href="#"
+              onClick={() => onPageChange(page)}
+            >
               {page}
             </a>
           </li>
