b11212a0165f3d6e916a6fc7db461bcb032e12be
Abdul Wahab <abwahab708@gmail.com>
2019-10-31 11:16:01 +0500

    Sorting - Extracting TableHeader
    We need to extract a new component like TableHeader that encapsulates the sort algo/logic and then we can reuse that TableHeader whenever we've a table like table of movies, customers, rentals and so on.

---
+++ components/vidly/src/components/common/tableHeader.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+// columns: array
+// we should also pass sortColumn: Object
+// onSort: function
+
+class TableHeader extends Component {
+  raiseSort = path => {
+    const sortColumn = { ...this.props.sortColumn }; // cloning
+    if (sortColumn.path === path)
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    // updating order when path is same
+    else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.props.onSort(sortColumn);
+  };
+
+  render() {
+    return (
+      <thead>
+        <tr>
+          {this.props.columns.map(column => (
+            <th
+              key={column.path || column.key}
+              onClick={() => this.raiseSort(column.path)}
+            >
+              {column.label}
+            </th>
+          ))}
+        </tr>
+      </thead>
+    );
+  }
+}
+
+export default TableHeader;
