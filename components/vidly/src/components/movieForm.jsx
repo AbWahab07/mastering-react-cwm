da75f012fd4ecbbd94756fa812352ae57e6b3b3d
Abdul Wahab <abwahab708@gmail.com>
2019-11-02 13:05:00 +0500

    Vidly - Linking to the Movie Form
    To add the link to the title, we've to modify the columns array in MoviesTable component.

--- components/vidly/src/components/movieForm.jsx
+++ components/vidly/src/components/movieForm.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
-const MovieForm = () => {
-  return <h1>Movie Form</h1>;
+const MovieForm = ({ match, history }) => {
+  /* Can't add a helper function as we've a functional component
+  handleSave = ({ history }) => {
+    history.push("/movies");
+  };
+  */
+  return (
+    <div>
+      <h1>Movie Form - {match.params.id}</h1>
+      <button
+        className="btn btn-primary"
+        onClick={() => history.push("/movies")}
+      >
+        Save
+      </button>
+    </div>
+  );
 };
 
 export default MovieForm;
