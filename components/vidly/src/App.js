e6b2a5cde5437baa7aa8be1612d7a76f8651fc4d
Abdul Wahab <abwahab708@gmail.com>
2019-11-02 12:05:57 +0500

    Vidly - Adding Routes
    Added App component to index and wrapped it with BrowserRouter.
    Defined Routes in the App component. Used switch to render the first child in Routes that matches the location, so the other Routes will be ignored.
    Used Redirect to redirect the homepage to the movies component and to the NotFound component

--- components/vidly/src/App.js
+++ components/vidly/src/App.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movie";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound.jsx";
 
-function App() {
-  return (
-    <main className="container">
-      <h1>Hello World</h1>
-    </main>
-  );
+class App extends Component {
+  render() {
+    return (
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
+    );
+  }
 }
 
 export default App;
