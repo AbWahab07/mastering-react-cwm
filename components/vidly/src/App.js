4f4aaf21307ddbb3fbf423b6ee287a4df1b42595
Abdul Wahab <abwahab708@gmail.com>
2019-11-02 12:22:21 +0500

    Vidly - Adding the NavBar
    Added the NavBar component using bootstrap snippet. Used Link and NavLink instead of anchor tags.
    NavLink automatically highlights the active url/link. Used NavBar in the App component and used React.Fragment as we've two childs.

--- components/vidly/src/App.js
+++ components/vidly/src/App.js
@@ -5,20 +5,24 @@ import Movies from "./components/movie";
 import Customers from "./components/customers";
 import Rentals from "./components/rentals";
 import NotFound from "./components/notFound.jsx";
+import NavBar from "./components/common/navBar";
 
 class App extends Component {
   render() {
     return (
-      <main className="container">
-        <Switch>
-          <Route path="/movies" component={Movies}></Route>
-          <Route path="/customers" component={Customers}></Route>
-          <Route path="/rentals" component={Rentals}></Route>
-          <Route path="/not-found" component={NotFound}></Route>
-          <Redirect from="/" exact to="/movies" />
-          <Redirect to="/not-found" />
-        </Switch>
-      </main>
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/movies" component={Movies}></Route>
+            <Route path="/customers" component={Customers}></Route>
+            <Route path="/rentals" component={Rentals}></Route>
+            <Route path="/not-found" component={NotFound}></Route>
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
     );
   }
 }
