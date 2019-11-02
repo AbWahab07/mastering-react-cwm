5d24b226b4d8c3dcaad969d465432ccb7ff3bccf
Abdul Wahab <abwahab708@gmail.com>
2019-11-02 14:05:06 +0500

    Forms- Building Bootstrap Form
    Created LoginFrom component using bootstrap.
    Added a Route for LoginForm component in App component.
    Added a Link for LoginForm component in NavBar component.

--- components/vidly/src/App.js
+++ components/vidly/src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import "./App.css";
 import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movie";
 import Customers from "./components/customers";
@@ -7,6 +6,8 @@ import Rentals from "./components/rentals";
 import NotFound from "./components/notFound.jsx";
 import NavBar from "./components/common/navBar";
 import MovieForm from "./components/movieForm";
+import LoginForm from "./components/loginForm";
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -15,6 +16,7 @@ class App extends Component {
         <NavBar />
         <main className="container">
           <Switch>
+            <Route path="/login" component={LoginForm} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" component={Movies}></Route>
             <Route path="/customers" component={Customers}></Route>
