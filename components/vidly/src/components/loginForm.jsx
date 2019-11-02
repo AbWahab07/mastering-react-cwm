6e694d31df35684e7a0e6a31c2728b78d5247ad3
Abdul Wahab <abwahab708@gmail.com>
2019-11-02 14:44:39 +0500

    Refs - Access DOM element
    When submitting the form we need values of input fields. In order to get access to a DOM element(input in our case) we need to give it a reference.
    You should not directly access the Document object to get access to input field, use React ref instead.
    As a rule of thumb, you should minizme the use of refs, use it only when you know whar you're doing.
    Used ref to set focus on a given input field.

--- components/vidly/src/components/loginForm.jsx
+++ components/vidly/src/components/loginForm.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
+  username = React.createRef(); // creating ref for username field
+
+  // componentDidMount() {
+  //   this.username.current.focus(); // gives focus to the input field, we can use autoFoucs attribute to avoid this
+  // }
+
   handleSubmit = e => {
     e.preventDefault();
 
     // Call the server
+
+    // const username = document.getElementById('username').value;  you shouldn't access the DOM using document object model
+    const username = this.username.current.value; // accessing username using ref
     console.log("Submitted");
   };
 
@@ -15,7 +24,13 @@ class LoginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className="form-group">
             <label htmlFor="username">UserName</label>
-            <input id="username" type="text" className="form-control" />
+            <input
+              autoFocus
+              ref={this.username}
+              id="username"
+              type="text"
+              className="form-control"
+            />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
