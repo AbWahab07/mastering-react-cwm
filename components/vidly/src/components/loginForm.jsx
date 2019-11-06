06c02136d338050ae26ead29d69d8edddbeeae88
Abdul Wahab <abwahab708@gmail.com>
2019-11-06 08:49:39 +0500

    Validating a Form Using Joi
    Added label method of Joi to improve the messaging of errors.
    Used for of loop to map errors object.
    Destructured error object from validate method. Defined options object to simplify the validate method.

--- components/vidly/src/components/loginForm.jsx
+++ components/vidly/src/components/loginForm.jsx
@@ -10,27 +10,25 @@ class LoginForm extends Component {
   };
 
   schema = {
-    username: Joi.string().required(),
-    password: Joi.string().required()
+    username: Joi.string()
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .required()
+      .label("Password")
   };
 
   // returns an errors object to set errors in the state
   validate = () => {
-    const result = Joi.validate(this.state.account, this.schema, {
-      abortEarly: false
-    });
-    console.log(result);
+    const options = { abortEarly: false };
+    const { error } = Joi.validate(this.state.account, this.schema, options);
+    // console.log(error);
 
     const errors = {};
-
-    const { account } = this.state;
-    // if username or password is empty
-    if (account.username.trim() === "")
-      errors.username = "Username is required";
-    if (account.password.trim() === "")
-      errors.password = "Password is required";
-
-    return Object.keys(errors).length === 0 ? null : errors;
+    // if error is falsy
+    if (!error) return null;
+    for (let item of error.details) errors[item.path[0]] = item.message;
+    return errors;
   };
 
   handleSubmit = e => {
