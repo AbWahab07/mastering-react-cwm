751cc565c39da153c718bf21213e5b82ffeda75c
Abdul Wahab <abwahab708@gmail.com>
2019-11-11 11:50:10 +0500

    Extracting a Config File
    Added a config.json file with apiUrl
    Refactored genreService and movieService to use this config file for the api endpoint

--- components/vidly/src/services/movieService.js
+++ components/vidly/src/services/movieService.js
@@ -1,6 +1,7 @@
 import http from "./httpService";
+import { apiUrl } from "../config.json";
 
-const apiEndPoint = "http://localhost:3900/api/movies";
+const apiEndPoint = apiUrl + "/movies";
 export function getMovies() {
   return http.get(apiEndPoint);
 }
